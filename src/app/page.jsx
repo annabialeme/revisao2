5f3f6ffa49d5f070528c65becd7209a6aa703f00
annabialeme <annaleme.tds1.senai@gmail.com>
2025-08-05 16:22:52 -0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from "../components/Header/Header";
+
+export default function Page() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>🛒 Loja de Eletrônicos</h1>
+      <p>Os melhores produtos com os melhores preços!</p>
+      <p>📊 Total de produtos: <span id="totalProducts">0</span></p>
+    </div>
+  );
+};
